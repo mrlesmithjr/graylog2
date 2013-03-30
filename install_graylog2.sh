96403b8233a3ad13b4a98b40600aadcd21e3491f
mrlesmithjr <larry.e.smith.jr@gmail.com>
2013-03-30 12:07:28 -0300

    Update install_graylog2.sh

--- install_graylog2.sh
+++ install_graylog2.sh
@@ -24,7 +24,7 @@ SERVERALIAS=$IPADDY
 
 echo Updating Apt Packages and Installing Pre-Reqs
 sudo apt-get -qq update
-sudo apt-get -y install git curl apache2 libcurl4-openssl-dev apache2-prefork-dev libapr1-dev libcurl4-openssl-dev apache2-prefork-dev libapr1-dev build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config python-software-properties
+sudo apt-get -y install git curl apache2 libcurl4-openssl-dev apache2-prefork-dev libapr1-dev libcurl4-openssl-dev apache2-prefork-dev libapr1-dev build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config python-software-properties apt-file
 
 # Install Oracle Java 7
 echo Installing Oracle Java 7
