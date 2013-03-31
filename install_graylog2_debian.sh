2f1a915a1c18152cf315f9034d5c33f47014d0d5
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2013-03-31 13:09:16 -0300

    Update install_graylog2_debian.sh

--- install_graylog2_debian.sh
+++ install_graylog2_debian.sh
@@ -16,13 +16,13 @@ exec > >(tee "./graylog2/install_graylog2.log")
 #SERVERALIAS="x.x.x.x"
 #
 #
-echo Detecting IP Address
+echo "Detecting IP Address"
 IPADDY="$(sudo ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 
 SERVERNAME=$IPADDY
 SERVERALIAS=$IPADDY
 
-echo Disabling CD Sources and Updating Apt Packages and Installing Pre-Reqs
+echo "Disabling CD Sources and Updating Apt Packages and Installing Pre-Reqs"
 sudo sed -i -e 's|deb cdrom:|# deb cdrom:|' /etc/apt/sources.list
 sudo apt-get -qq update
 sudo apt-get -y install git curl apache2 libcurl4-openssl-dev apache2-prefork-dev libapr1-dev libcurl4-openssl-dev apache2-prefork-dev libapr1-dev build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config python-software-properties
@@ -32,9 +32,10 @@ sudo apt-get -y install git curl apache2 libcurl4-openssl-dev apache2-prefork-de
 #sudo apt-file update
 
 #Install Oracle Java 6
+echo "Installing Oracle Java 6"
 sudo apt-get install openjdk-6-jre
 
-echo Downloading Elasticsearch
+echo "Downloading Elasticsearch"
 
 git clone https://github.com/elasticsearch/elasticsearch-servicewrapper.git
 sudo chown -R $USER:$USER /usr/local/src
@@ -42,14 +43,14 @@ sudo chown -R $USER:$USER /usr/local/src
 cd /usr/local/src
 git clone https://github.com/elasticsearch/elasticsearch-servicewrapper.git
 
-echo Downloading Elastic Search, Graylog2-Server and Graylog2-Web-Interface to /usr/local/src
+echo "Downloading Elastic Search, Graylog2-Server and Graylog2-Web-Interface to /usr/local/src"
 
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.6.tar.gz
 wget http://download.graylog2.org/graylog2-server/graylog2-server-0.11.0.tar.gz
 wget http://download.graylog2.org/graylog2-web-interface/graylog2-web-interface-0.11.0.tar.gz
 
 #extract files
-echo Extracting Elasticsearch, Graylog2-Server and Graylog2-Web-Interface to /usr/local/src
+echo "Extracting Elasticsearch, Graylog2-Server and Graylog2-Web-Interface to /usr/local/src"
 
 for f in *.tar.gz
 do
@@ -57,12 +58,12 @@ tar zxf "$f"
 done
 
 # Create Symbolic Links
-echo Creating SymLinks for elasticsearch and graylog2-server
+echo "Creating SymLinks for elasticsearch and graylog2-server"
 ln -s elasticsearch-0.20.6/ elasticsearch
 ln -s graylog2-server-0.11.0/ graylog2-server
 
 #Install elasticsearch
-echo Installing elasticsearch
+echo "Installing elasticsearch"
 
 mv *servicewrapper*/service elasticsearch/bin/
 rm -Rf *servicewrapper*
@@ -75,7 +76,7 @@ sed -i -e 's|# cluster.name: elasticsearch|cluster.name: graylog2|' /usr/local/s
 # curl -XGET 'http://localhost:9200/_cluster/health?pretty=true'
 
 #Install mongodb
-echo Installing MongoDB
+echo "Installing MongoDB"
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen" | sudo tee /etc/apt/sources.list.d/10gen.list
@@ -83,7 +84,7 @@ sudo apt-get -qq update
 sudo apt-get -y install mongodb-10gen
 
 #Install graylog2-server
-echo Installing graylog2-server
+echo "Installing graylog2-server"
 
 cd graylog2-server-0.11.0/
 cp /usr/local/src/graylog2-server/elasticsearch.yml{.example,}
@@ -92,7 +93,7 @@ cp /usr/local/src/graylog2-server/graylog2.conf{.example,}
 sudo ln -s /usr/local/src/graylog2-server/graylog2.conf /etc/graylog2.conf
 sed -i -e 's|mongodb_useauth = true|mongodb_useauth = false|' /usr/local/src/graylog2-server/graylog2.conf
 
-echo Creating /etc/init.d/graylog2-server startup script
+echo "Creating /etc/init.d/graylog2-server startup script"
 
 (
 cat <<'EOF'
@@ -144,18 +145,18 @@ EOF
 sudo chmod +x /etc/init.d/graylog2-server
 
 #Start graylog2-server on bootup
-echo Making graylog2-server startup on boot
+echo "Making graylog2-server startup on boot"
 
 sudo update-rc.d graylog2-server defaults
 
 #Install graylog2 web interface
-echo Installing graylog2-web-interface
+echo "Installing graylog2-web-interface"
 
 cd /usr/local/src/
 ln -s graylog2-web-interface-0.11.0 graylog2-web-interface
 
 #Install Ruby
-echo Installing Ruby
+echo "Installing Ruby"
 
 sudo apt-get -y install libgdbm-dev libffi-dev
 \curl -L https://get.rvm.io | bash -s stable
@@ -163,14 +164,14 @@ source /home/$USER/.rvm/scripts/rvm
 rvm install 1.9.2
 
 #Install Gems
-echo Installing Ruby Gems
+echo "Installing Ruby Gems"
 
 cd /usr/local/src/graylog2-web-interface
 gem install bundler --no-ri --no-rdoc
 bundle install
 
 #Set MongoDB Settings
-echo Configuring MongoDB
+echo "Configuring MongoDB"
 
 echo "
 production:
@@ -181,7 +182,7 @@ production:
  database: graylog2" | tee /usr/local/src/graylog2-web-interface/config/mongoid.yml
 
 #Create MongoDB Users and Set Passwords
-echo Creating MongoDB Users and Passwords
+echo "Creating MongoDB Users and Passwords"
 
 mongo admin --eval "db.addUser('admin', 'password123')"
 mongo admin --eval "db.auth('admin', 'password123')"
@@ -193,26 +194,26 @@ mongo graylog2 --eval "db.auth('grayloguser', 'password123')"
 #RAILS_ENV=production script/rails server
 
 # Install Apache-passenger
-echo Installing Apache-Passenger Modules
+echo "Installing Apache-Passenger Modules"
 
 gem install passenger
 passenger-install-apache2-module
 
 #Add passenger code
-echo Adding Apache Passenger modules to /etc/apache2/httpd.conf
+echo "Adding Apache Passenger modules to /etc/apache2/httpd.conf"
 
 echo "LoadModule passenger_module /home/$USER/.rvm/gems/ruby-1.9.2-p320/gems/passenger-3.0.18/ext/apache2/mod_passenger.so" | sudo tee -a /etc/apache2/httpd.conf
 echo "PassengerRoot /home/$USER/.rvm/gems/ruby-1.9.2-p320/gems/passenger-3.0.18" | sudo tee -a /etc/apache2/httpd.conf
 echo "PassengerRuby /home/$USER/.rvm/wrappers/ruby-1.9.2-p320/ruby" | sudo tee -a /etc/apache2/httpd.conf
 
 #Restart Apache2
-echo Restarting Apache2
+echo "Restarting Apache2"
 
 sudo /etc/init.d/apache2 restart
 #If apache fails and complains about unable to load mod_passenger.so check and verify that your passengerroot version matches
 
 #Configure virtualhost
-echo Configuring Apache VirtualHost
+echo "Configuring Apache VirtualHost"
 
 echo "
 <VirtualHost *:80>
@@ -229,19 +230,19 @@ CustomLog /var/log/apache2/access.log combined
 </VirtualHost>" | sudo tee /etc/apache2/sites-available/graylog2
 
 # Enable virtualhost
-echo Enabling Apache VirtualHost Settings
+echo "Enabling Apache VirtualHost Settings"
 
 sudo a2dissite 000-default
 sudo a2ensite graylog2
 sudo service apache2 reload
 
 # Restart apache
-echo Restarting Apache2
+echo "Restarting Apache2"
 
 sudo /etc/init.d/apache2 restart
 
 #Now we need to modify some things to get rsyslog to forward to graylog. this is useful for ESXi syslog format to be correct.
-echo Updating graylog2.conf, rsyslog.conf
+echo "Updating graylog2.conf, rsyslog.conf"
 
 sudo sed -i -e 's|syslog_listen_port = 514|syslog_listen_port = 10514|' /etc/graylog2.conf
 sudo sed -i -e 's|mongodb_password = 123|mongodb_password = password123|' /etc/graylog2.conf
@@ -257,7 +258,7 @@ echo '$PreserveFQDN on' | sudo tee -a  /etc/rsyslog.d/32-graylog2.conf
 echo '*.* @localhost:10514' | sudo tee -a  /etc/rsyslog.d/32-graylog2.conf
 
 #Restart All Services
-echo Restarting All Services Required for Graylog2 to work
+echo "Restarting All Services Required for Graylog2 to work"
 
 sudo service elasticsearch restart
 sudo service mongodb restart
@@ -266,10 +267,11 @@ sudo service rsyslog restart
 sudo service apache2 restart
 
 #All Done
-echo Installation has completed!!
-echo Browse to IP address of this Graylog2 Server Used for Installation
-echo IP Address detected from system is $IPADDY
-echo Browse to http://$IPADDY
-echo You Entered $SERVERNAME During Install
-echo Browse to http://$SERVERNAME If Different
-echo EveryThingShouldBeVirtual.com
+echo "Installation has completed!!"
+echo "Browse to IP address of this Graylog2 Server Used for Installation"
+echo "IP Address detected from system is $IPADDY"
+echo "Browse to http://$IPADDY"
+echo "You Entered $SERVERNAME During Install"
+echo "Browse to http://$SERVERNAME If Different"
+echo "EveryThingShouldBeVirtual.com"
+echo "@mrlesmithjr"
