87f6bd4ecb10f323d38dcd96c310aa4c2f977d63
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2013-04-02 21:19:33 -0300

    Update install_graylog2_ubuntu_1210.sh

--- install_graylog2_ubuntu_1210.sh
+++ install_graylog2_ubuntu_1210.sh
@@ -29,10 +29,6 @@ sudo sed -i -e 's|deb cdrom:|# deb cdrom:|' /etc/apt/sources.list
 sudo apt-get -qq update
 sudo apt-get -y install git curl apache2 libcurl4-openssl-dev apache2-prefork-dev libapr1-dev libcurl4-openssl-dev apache2-prefork-dev libapr1-dev build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config python-software-properties
 
-#Debian 6.0 Testing of all-in-one script
-#sudo apt-get -y install apt-file
-#sudo apt-file update
-
 # Install Oracle Java 7
 echo "Installing Oracle Java 7"
 sudo add-apt-repository -y ppa:webupd8team/java
