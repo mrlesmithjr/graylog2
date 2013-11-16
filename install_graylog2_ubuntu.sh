153150d7f5fb4e3c4b134c776d58cc6d3537a9e9
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2013-11-16 01:04:45 -0500

    Update install_graylog2_ubuntu.sh

--- install_graylog2_ubuntu.sh
+++ install_graylog2_ubuntu.sh
@@ -196,12 +196,12 @@ mongo graylog2 --eval "db.auth('grayloguser', 'password123')"
 # Install Apache-passenger
 echo Installing Apache-Passenger Modules
 gem install passenger
-/var/lib/gems/1.9.1/gems/passenger-4.0.23/bin/passenger-install-apache2-module --auto
+/var/lib/gems/1.9.1/gems/passenger-4.0.24/bin/passenger-install-apache2-module --auto
 
 # Add passenger modules for Apache2
 echo "Adding Apache Passenger modules to /etc/apache2/httpd.conf"
-echo "LoadModule passenger_module /var/lib/gems/1.9.1/gems/passenger-4.0.23/buildout/apache2/mod_passenger.so" | tee -a /etc/apache2/mods-available/passenger.load
-echo "PassengerRoot /var/lib/gems/1.9.1/gems/passenger-4.0.23" | tee -a /etc/apache2/mods-available/passenger.conf
+echo "LoadModule passenger_module /var/lib/gems/1.9.1/gems/passenger-4.0.24/buildout/apache2/mod_passenger.so" | tee -a /etc/apache2/mods-available/passenger.load
+echo "PassengerRoot /var/lib/gems/1.9.1/gems/passenger-4.0.24" | tee -a /etc/apache2/mods-available/passenger.conf
 echo "PassengerRuby /usr/bin/ruby1.9.1" | tee -a /etc/apache2/mods-available/passenger.conf
 
 # Enable passenger modules
