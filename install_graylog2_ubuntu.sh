844db61af7d8830afb557d330eb737d8a782b791
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2013-11-23 17:29:33 -0500

    Update install_graylog2_ubuntu.sh
    Changed from Oracle Java to openjdk-7-jre

--- install_graylog2_ubuntu.sh
+++ install_graylog2_ubuntu.sh
@@ -42,14 +42,14 @@ sed -i -e 's|deb cdrom:|# deb cdrom:|' /etc/apt/sources.list
 apt-get -qq update
 
 # Install Pre-Reqs
-apt-get -y install git curl apache2 libcurl4-openssl-dev apache2-prefork-dev libapr1-dev libcurl4-openssl-dev apache2-prefork-dev libapr1-dev build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config python-software-properties software-properties-common
+apt-get -y install git curl apache2 libcurl4-openssl-dev apache2-prefork-dev libapr1-dev libcurl4-openssl-dev apache2-prefork-dev libapr1-dev build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config python-software-properties software-properties-common openjdk-7-jre
 
-# Install Oracle Java 7
-echo "Installing Oracle Java 7"
-add-apt-repository -y ppa:webupd8team/java
-apt-get -qq update
-echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-apt-get -y install oracle-java7-installer
+# Install Oracle Java 7 **NOT Used - Installing openjdk-7-jre above
+# echo "Installing Oracle Java 7"
+# add-apt-repository -y ppa:webupd8team/java
+# apt-get -qq update
+# echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+# apt-get -y install oracle-java7-installer
 
 echo "Downloading Elasticsearch"
 # chown -R $USER:$USER /opt
