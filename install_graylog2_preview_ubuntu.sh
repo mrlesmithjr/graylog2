1dcb10cb3c6a5431efd2320f33840788620688fc
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2013-11-25 15:05:24 -0500

    Update install_graylog2_preview_ubuntu.sh
    Cleaning up code

--- install_graylog2_preview_ubuntu.sh
+++ install_graylog2_preview_ubuntu.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 #Provided by @mrlesmithjr
 #EveryThingShouldBeVirtual.com
-#
-#
+
 # Ubuntu Install Script
-#
+
 set -e
 # Setup logging
 # Logs stderr and stdout to separate files.
 exec 2> >(tee "./graylog2/install_graylog2.err")
 exec > >(tee "./graylog2/install_graylog2.log")
-#
+
 # Checking if running as root (10/16/2013 - No longer an issue - Should be ran as root or with sudo)
 # Do not run as root
 # if [[ $EUID -eq 0 ]];then
@@ -20,21 +19,12 @@ exec > >(tee "./graylog2/install_graylog2.log")
 # exit 1
 # fi
 
-# Apache Settings
-# change x.x.x.x to whatever your ip address is of the server you are installing on or let the script auto detect your IP
-# which is the default
-# SERVERNAME="x.x.x.x"
-# SERVERALIAS="x.x.x.x"
-#
-#
 echo "Detecting IP Address"
 IPADDY="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 echo "Detected IP Address is $IPADDY"
 
 SERVERNAME=$IPADDY
 SERVERALIAS=$IPADDY
-
-#Ruby Passenger Version
 
 # Disable CD Sources in /etc/apt/sources.list
 echo "Disabling CD Sources and Updating Apt Packages and Installing Pre-Reqs"
@@ -44,39 +34,28 @@ apt-get -qq update
 # Install Pre-Reqs
 apt-get -y install git curl libcurl4-openssl-dev libapr1-dev libcurl4-openssl-dev libapr1-dev build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config python-software-properties software-properties-common openjdk-7-jre pwgen
 
-
-echo "Downloading Elasticsearch"
-# chown -R $USER:$USER /opt
-cd /opt
-#git clone https://github.com/elasticsearch/elasticsearch-servicewrapper.git
-
 # Download Elasticsearch, Graylog2-Server and Graylog2-Web-Interface
 echo "Downloading Elastic Search, Graylog2-Server and Graylog2-Web-Interface to /opt"
+cd /opt
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb
 wget https://github.com/Graylog2/graylog2-server/releases/download/0.20.0-preview.6/graylog2-server-0.20.0-preview.6.tgz
 wget https://github.com/Graylog2/graylog2-web-interface/releases/download/0.20.0-preview.6/graylog2-web-interface-0.20.0-preview.6.tgz
 
 # Extract files
-echo "Extracting Elasticsearch, Graylog2-Server and Graylog2-Web-Interface to /opt"
+echo "Extracting Graylog2-Server and Graylog2-Web-Interface to /opt"
   for f in *.*gz
 do
 tar zxf "$f"
 done
 
 # Create Symbolic Links
-echo "Creating SymLinks for elasticsearch and graylog2-server"
-#ln -s elasticsearch-0.90*/ elasticsearch
+echo "Creating SymLink Graylog2-server"
 ln -s graylog2-server-0.2*/ graylog2-server
 
 # Install elasticsearch
 echo "Installing elasticsearch"
 dpkg -i elasticsearch-0.90.7.deb
-#mv *servicewrapper*/service elasticsearch/bin/
-#rm -Rf *servicewrapper*
-#/opt/elasticsearch/bin/service/elasticsearch install
-#ln -s `readlink -f elasticsearch/bin/service/elasticsearch` /usr/bin/elasticsearch_ctl
 sed -i -e 's|# cluster.name: elasticsearch|cluster.name: graylog2|' /etc/elasticsearch/elasticsearch.yml
-#/etc/init.d/elasticsearch start
 
 # Test elasticsearch
 # curl -XGET 'http://localhost:9200/_cluster/health?pretty=true'
@@ -91,8 +70,6 @@ apt-get -y install mongodb-10gen
 # Install graylog2-server
 echo "Installing graylog2-server"
 cd graylog2-server/
-#cp /opt/graylog2-server/elasticsearch.yml{.example,}
-#ln -s /opt/graylog2-server/elasticsearch.yml /etc/graylog2-elasticsearch.yml
 cp /opt/graylog2-server/graylog2.conf{.example,}
 ln -s /opt/graylog2-server/graylog2.conf /etc/graylog2.conf
 pass_secret=$(pwgen -s 96)
@@ -102,192 +79,47 @@ sed -i -e 's|elasticsearch_shards = 4|elasticsearch_shards = 1|' /opt/graylog2-s
 sed -i -e 's|mongodb_useauth = true|mongodb_useauth = false|' /opt/graylog2-server/graylog2.conf
 /opt/graylog2-server/bin/graylog2ctl start
 
-# Create graylog2-server startup script
-#echo "Creating /etc/init.d/graylog2-server startup script"
-#(
-#cat <<'EOF'
-#!/bin/sh
-#
-# graylog2-server: graylog2 message collector
-#
-# chkconfig: - 98 02
-# description: This daemon listens for syslog and GELF messages and stores them in mongodb
-#
-#CMD=$1
-#NOHUP=`which nohup`
-#JAVA_CMD=/usr/bin/java
-#GRAYLOG2_SERVER_HOME=/opt/graylog2-server
-#start() {
-# echo "Starting graylog2-server ..."
-#$NOHUP $JAVA_CMD -jar $GRAYLOG2_SERVER_HOME/graylog2-server.jar > /var/log/graylog2.log 2>&1 &
-#}
-
-#stop() {
-#PID=`cat /tmp/graylog2.pid`
-#echo "Stopping graylog2-server ($PID) ..."
-#kill $PID
-#}
-
-#restart() {
-#echo "Restarting graylog2-server ..."
-#stop
-#start
-#}
-
-#case "$CMD" in
-#start)
-#start
-#;;
-#stop)
-#stop
-#;;
-#restart)
-#restart
-#;;
-#*)
-#echo "Usage $0 {start|stop|restart}"
-#RETVAL=1
-#esac
-#EOF
-#) | tee /etc/init.d/graylog2-server
-
-# Make graylog2-server executable
-#chmod +x /etc/init.d/graylog2-server
-
-# Start graylog2-server on bootup
-#echo "Making graylog2-server startup on boot"
-#update-rc.d graylog2-server defaults
-
 # Install graylog2 web interface
 echo "Installing graylog2-web-interface"
 cd /opt/
 ln -s graylog2-web-interface-0.2*/ graylog2-web-interface
-# mkdir  /opt/graylog2-web-interface/tmp/
-
-# Install Ruby
-#echo "Installing Ruby"
-#apt-get -y install libgdbm-dev libffi-dev ruby1.9.3
-
-# Install Ruby Gems
-#echo "Installing Ruby Gems"
-#cd /opt/graylog2-web-interface
-#gem install bundler --no-ri --no-rdoc
-#bundle install
-
-# Set MongoDB Settings
-#echo "Configuring MongoDB"
-#echo "
-#production:
-# host: localhost
-# port: 27017
-# username: grayloguser
-# password: password123
-# database: graylog2" | tee /opt/graylog2-web-interface/config/mongoid.yml
-
-# Create MongoDB Users and Set Passwords
-#echo "Creating MongoDB Users and Passwords"
-#mongo admin --eval "db.addUser('admin', 'password123')"
-#mongo admin --eval "db.auth('admin', 'password123')"
-#mongo graylog2 --eval "db.addUser('grayloguser', 'password123')"
-#mongo graylog2 --eval "db.auth('grayloguser', 'password123')"
-
-# Test Install
-# cd /opt/graylog2-web-interface
-# RAILS_ENV=production script/rails server
-
-# Install Apache-passenger
-#echo "Installing Apache-Passenger Modules"
-#gem install passenger
-# Create Passenger symbolic link to get around versions changing
-#ln -s /var/lib/gems/1.9.1/gems/passenger-4* /var/lib/gems/1.9.1/gems/passenger
-# Build Apache2 passenger module
-#/var/lib/gems/1.9.1/gems/passenger/bin/passenger-install-apache2-module --auto
-
-# Add passenger modules for Apache2
-#echo "Adding Apache Passenger modules to /etc/apache2/httpd.conf"
-#echo "LoadModule passenger_module /var/lib/gems/1.9.1/gems/passenger/buildout/apache2/mod_passenger.so" | tee -a /etc/apache2/mods-available/passenger.load
-#echo "PassengerRoot /var/lib/gems/1.9.1/gems/passenger" | tee -a /etc/apache2/mods-available/passenger.conf
-#echo "PassengerRuby /usr/bin/ruby1.9.1" | tee -a /etc/apache2/mods-available/passenger.conf
-
-# Enable passenger modules
-#echo "Enabling Apache Passenger module"
-#a2enmod passenger
-
-# Restart Apache2
-#echo "Restarting Apache2"
-#service apache2 restart
-# If apache fails and complains about unable to load mod_passenger.so check and verify that your passengerroot version matches
-
-# Configure Apache virtualhost
-#echo "Configuring Apache VirtualHost"
-#echo "
-#<VirtualHost *:80>
-#ServerName ${SERVERNAME}
-#ServerAlias ${SERVERALIAS}
-#DocumentRoot /opt/graylog2-web-interface/public
-
-#Allow from all
-#Options -MultiViews
-
-#ErrorLog /var/log/apache2/error.log
-#LogLevel warn
-#CustomLog /var/log/apache2/access.log combined
-#</VirtualHost>" | tee /etc/apache2/sites-available/graylog2
-
-# Enable virtualhost
-#echo "Enabling Apache VirtualHost Settings"
-#a2dissite 000-default
-#a2ensite graylog2
-#service apache2 reload
-
-# Restart apache
-#echo "Restarting Apache2"
-#service apache2 restart
 
 # Now we need to modify some things to get rsyslog to forward to graylog. this is useful for ESXi syslog format to be correct.
-echo "Updating graylog2.conf, rsyslog.conf"
+echo "Updating graylog2.conf and rsyslog.conf"
 sed -i -e 's|syslog_listen_port = 514|syslog_listen_port = 10514|' /etc/graylog2.conf
-#sed -i -e 's|mongodb_password = 123|mongodb_password = password123|' /etc/graylog2.conf
 sed -i -e 's|#$ModLoad immark|$ModLoad immark|' /etc/rsyslog.conf
 sed -i -e 's|#$ModLoad imudp|$ModLoad imudp|' /etc/rsyslog.conf
 sed -i -e 's|#$UDPServerRun 514|$UDPServerRun 514|' /etc/rsyslog.conf
 sed -i -e 's|#$ModLoad imtcp|$ModLoad imtcp|' /etc/rsyslog.conf
 sed -i -e 's|#$InputTCPServerRun 514|$InputTCPServerRun 514|' /etc/rsyslog.conf
 sed -i -e 's|*.*;auth,authpriv.none|#*.*;auth,authpriv.none|' /etc/rsyslog.d/50-default.conf
-# echo '$template GRAYLOG2,"<%PRI%>1 %timegenerated:::date-rfc3339% %HOSTNAME% %syslogtag% - %APP-NAME%: %msg:::drop-last-lf%\n"' | tee /etc/rsyslog.d/32-graylog2.conf
 echo '$template GRAYLOG2,"<%PRI%>1 %timegenerated:::date-rfc3339% %FROMHOST% %syslogtag% - %APP-NAME%: %msg:::drop-last-lf%\n"' | tee /etc/rsyslog.d/32-graylog2.conf
 echo '$ActionForwardDefaultTemplate GRAYLOG2' | tee -a  /etc/rsyslog.d/32-graylog2.conf
 echo '$PreserveFQDN on' | tee -a  /etc/rsyslog.d/32-graylog2.conf
-#echo '*.err;*.crit;*.alert;*.emerg;cron.*;auth,authpriv.* @localhost:10514' | tee -a  /etc/rsyslog.d/32-graylog2.conf
-# Log syslog levels info and above
 echo '*.info @localhost:10514' | tee -a  /etc/rsyslog.d/32-graylog2.conf
-
-# Fixing issue with secret_token in /opt/graylog2-web-interface/config/initializers/secret_token.rb
-#sed -i -e "s|Graylog2WebInterface::Application.config.secret_token = 'CHANGE ME'|Graylog2WebInterface::Application.config.secret_token = 'b356d1af93673e37d6e21399d033d77c15354849fdde6d83fa0dca19608aa71f2fcd9d1f2784fb95e9400d8eeaf6dd9584d8d35b8f0b5c231369a70aac5e5777'|" /opt/graylog2-web-interface/config/initializers/secret_token.rb
 sed -i -e 's|graylog2-server.uris=""|graylog2-server.uris="http://127.0.0.1:12900/"|' /opt/graylog2-web-interface/conf/graylog2-web-interface.conf
 app_secret=$(pwgen -s 96)
 sed -i -e 's|application.secret=""|application.secret="'$app_secret'"|' /opt/graylog2-web-interface/conf/graylog2-web-interface.conf
+
+# Starting Graylog2 Web Interface
 nohup /opt/graylog2-web-interface/bin/graylog2-web-interface &
 
 # Fixing /opt/graylog2-web-interface Permissions
 echo "Fixing Graylog2 Web Interface Permissions"
-#chown -R root:root /opt/elasticsearch*
 chown -R root:root /opt/graylog2*
 chown -R www-data:www-data /opt/graylog2-web-interface*
 
 # Cleaning up /opt
 echo "Cleaning up"
-#rm /opt/elasticsearch*.*gz
 rm /opt/graylog2-server*.*gz
 rm /opt/graylog2-web-interface*.*gz
+rm /opt/elasticsearch-0.90.7.deb
 
 # Restart All Services
 echo "Restarting All Services Required for Graylog2 to work"
 service elasticsearch restart
 service mongodb restart
-#service graylog2-server restart
 service rsyslog restart
-#service apache2 restart
 
 # All Done
 echo "Installation has completed!!"
