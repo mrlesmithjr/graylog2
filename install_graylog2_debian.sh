9bc280e77f866a4864c46ed17bec4c9ac6720838
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2014-02-09 09:39:42 -0500

    Update install_graylog2_debian.sh
    removing sudo commands

--- install_graylog2_debian.sh
+++ install_graylog2_debian.sh
@@ -90,8 +90,8 @@ sed -i -e 's|# cluster.name: elasticsearch|cluster.name: graylog2|' /opt/elastic
 echo "Installing MongoDB"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen" | sudo tee /etc/apt/sources.list.d/10gen.list
-sudo apt-get -qq update
-sudo apt-get -y install mongodb-10gen
+apt-get -qq update
+apt-get -y install mongodb-10gen
 
 # Install graylog2-server
 echo "Installing graylog2-server"
