c51d96d7d31253bd6c426fe94bfd3a70947a5780
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2014-02-23 00:14:11 -0500

    Update upgrade_to_graylog2_20_ubuntu.sh

--- upgrade_to_graylog2_20_ubuntu.sh
+++ upgrade_to_graylog2_20_ubuntu.sh
@@ -11,10 +11,10 @@ exec > >(tee "./graylog2/upgrade_graylog2.log")
 
 service graylog2-web-interface stop
 rm /etc/init.d/graylog2-web-interface
-update-rc.d graylog2-web-interface remove -f
+update-rc.d graylog2-web-interface remove
 service graylog2-server stop
 rm /etc/init.d/graylog2-server
-update-rc.d graylog2-server remove -f
+update-rc.d graylog2-server remove
 rm /etc/graylog2.conf
 rm -rf /opt/graylog2-*
 # rm /etc/graylog2-server-node-id
