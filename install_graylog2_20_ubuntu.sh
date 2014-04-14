c5e079b07fb39cfa6cef90aeac9a01b1b02a7c73
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2014-04-14 17:06:34 -0400

    Update install_graylog2_20_ubuntu.sh

--- install_graylog2_20_ubuntu.sh
+++ install_graylog2_20_ubuntu.sh
@@ -70,7 +70,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee /etc/apt/sources.list.d/10gen.list
 apt-get -qq update
 apt-get -y install mongodb-10gen
-sed -i -e 's|#noprealloc = true|noprealloc = false|' /etc/mongodb.conf
+sed -i -e 's|#noprealloc = true|noprealloc = true|' /etc/mongodb.conf
 service mongodb restart
 
 # Making changes to /etc/security/limits.conf to allow more open files for elasticsearch
