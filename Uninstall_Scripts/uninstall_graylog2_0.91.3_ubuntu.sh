497f05617ad8e65ce67b5abb33dd602492e59054
Jason Jiho Kim <jason.kim.jiho@gmail.com>
2014-11-18 21:57:44 +0000

    added install/uninstall script for graylog2 v0.91.3

---
+++ Uninstall_Scripts/uninstall_graylog2_0.91.3_ubuntu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Provided by @mrlesmithjr
+# EveryThingShouldBeVirtual.com
+#
+# Ubuntu Graylog2 Preview/RC Uninstall Script
+set -e
+# Setup logging
+# Logs stderr and stdout to separate files.
+exec 2> >(tee "./graylog2/uninstall_graylog2.err")
+exec > >(tee "./graylog2/uninstall_graylog2.log")
+#
+service rsyslog stop
+service mongodb stop
+service elasticsearch stop
+service graylog2-web-interface stop
+rm /etc/init.d/graylog2-web-interface
+update-rc.d graylog2-web-interface remove
+#service graylog2-server stop
+/etc/init.d/graylog2-server stop
+rm /etc/init.d/graylog2-server
+update-rc.d graylog2-server remove
+rm /etc/graylog2.conf
+apt-get -y remove mongodb-10gen
+apt-get -y purge mongodb-10gen
+dpkg -r elasticsearch
+dpkg -P elasticsearch
+rm -rf /opt/graylog2-*
+rm /etc/graylog2-server-node-id
+rm -rf /var/lib/mongodb
