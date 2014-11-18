184dff60ae810ba8b0044c3b3627fbe47ede9841
Jason Jiho Kim <jason.kim.jiho@gmail.com>
2014-11-18 21:55:51 +0000

    reverting back

--- Uninstall_Scripts/uninstall_graylog2_preview_ubuntu.sh
+++ Uninstall_Scripts/uninstall_graylog2_preview_ubuntu.sh
@@ -9,13 +9,13 @@ set -e
 exec 2> >(tee "./graylog2/uninstall_graylog2.err")
 exec > >(tee "./graylog2/uninstall_graylog2.log")
 #
-#service rsyslog stop
-#service mongodb stop
-#service elasticsearch stop
-#service graylog2-web-interface stop
-#rm /etc/init.d/graylog2-web-interface
-#update-rc.d graylog2-web-interface remove
-#service graylog2-server stop
+service rsyslog stop
+service mongodb stop
+service elasticsearch stop
+service graylog2-web-interface stop
+rm /etc/init.d/graylog2-web-interface
+update-rc.d graylog2-web-interface remove
+service graylog2-server stop
 rm /etc/init.d/graylog2-server
 update-rc.d graylog2-server remove
 rm /etc/graylog2.conf
