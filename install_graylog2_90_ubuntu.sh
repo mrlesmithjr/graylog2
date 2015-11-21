a67f4f06607923708d787953002a09dc91a8882f
Frantzcy Paisible <frantzcy@paisible.com>
2015-11-21 08:55:54 -0500

    mongodb-10gen was not working for me, mongodb-org works fine (ubuntu 14.04)

--- install_graylog2_90_ubuntu.sh
+++ install_graylog2_90_ubuntu.sh
@@ -77,7 +77,7 @@ echo "Installing MongoDB"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee /etc/apt/sources.list.d/10gen.list
 apt-get -qq update
-apt-get -y install mongodb-10gen
+apt-get -y install mongodb-org
 
 # Waiting for MongoDB to start accepting connections on tcp/27017
 echo "!!!*** Waiting for MongoDB to start accepting connections ***!!!"
