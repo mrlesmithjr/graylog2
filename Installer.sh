dc42b6e88fb26d1a344d1672974a1b68ff544f37
Justin Looper <zeus-hacker@live.com>
2015-12-10 14:25:39 +0000

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,1 +1,19 @@
 #!/bin/bash -x
+
+# Install MondoDB
+
+sudo apt-get install mongodb-server
+
+# Install Elastic Search
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+sudo apt-get update && sudo apt-get install elasticsearch
+sudo update-rc.d elasticsearch defaults 95 10
+
+# Installing Graylog 2 Components
+
+wget https://packages.graylog2.org/repo/packages/graylog-1.3-repository-ubuntu14.04_latest.deb
+sudo dpkg -i graylog-1.3-repository-ubuntu14.04_latest.deb
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install graylog-server graylog-web
